6088ca144e0908c69ca0bf9c0ba844da340cf01b
Cong Thanh Vu <congthanh3181@gmail.com>
2023-08-05 17:32:31 +0700

    update configs

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,5 +1,5 @@
 export default {
-    PORTAL_STREAM_SERVICE_URL: 'http://localhost:3001',
+    PORTAL_STREAM_SERVICE_URL: 'https://sync.portal.cngthnh.io.vn',
     PORTAL_STREAM_START: '/stream/start',
-    PORTAL_STREAM_DASHBOARD: 'http://localhost:3000/stream/main'
+    PORTAL_STREAM_DASHBOARD: 'https://portal.cngthnh.io.vn/stream/main'
 }
